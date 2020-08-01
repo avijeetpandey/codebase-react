3e5c4597ae24751c18bf0d69ceedbe6761db5174
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-02 03:05:04 +0530

    Updates

---
+++ precache-manifest.a0d1adc8cb36b6107da4ddcf85b8f914.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877031a5fa36a444090ce1dbd7c4f065",
+    "url": "/codebase-react/index.html"
+  },
+  {
+    "revision": "680a52990147b5501583",
+    "url": "/codebase-react/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "ee557fa73ce36790cb30",
+    "url": "/codebase-react/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "680a52990147b5501583",
+    "url": "/codebase-react/static/js/2.7f1e0085.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/codebase-react/static/js/2.7f1e0085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee557fa73ce36790cb30",
+    "url": "/codebase-react/static/js/main.08081654.chunk.js"
+  },
+  {
+    "revision": "87ca4fb21f628c17a38e",
+    "url": "/codebase-react/static/js/runtime-main.584e92db.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/codebase-react/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/codebase-react/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/codebase-react/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/codebase-react/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/codebase-react/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/codebase-react/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/codebase-react/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/codebase-react/static/media/software.43049cae.jpeg"
+  }
+]);
